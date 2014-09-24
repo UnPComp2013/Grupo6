7b0f1c6854997a0c52b02b88bd6cc01e011817da
mayko <mayko_90@hotmail.com>
2014-09-24 12:46:42 -0300

    projeto

--- Mayko/projeto/login.h
+++ Mayko/projeto/login.h
@@ -1,13 +1,16 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
-#include <QDialog>
+#include <QMainWindow>
+#include <QtSql>
+#include <QtDebug>
+#include <QFileInfo>
 
 namespace Ui {
 class Login;
 }
 
-class Login : public QDialog
+class Login : public QMainWindow
 {
     Q_OBJECT
 
@@ -15,8 +18,13 @@ public:
     explicit Login(QWidget *parent = 0);
     ~Login();
 
+private slots:
+    void on_btn_ok_clicked();
+
 private:
     Ui::Login *ui;
+    QSqlDatabase mydb;
 };
 
 #endif // LOGIN_H
+
