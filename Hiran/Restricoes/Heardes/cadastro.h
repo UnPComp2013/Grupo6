dedbe4043e7e5d279016ecb627d1220692df44d9
Hiran <gondim.game@gmail.com>
2014-10-09 18:20:33 -0300

    Create cadastro.h

---
+++ Hiran/Restricoes/Heardes/cadastro.h
@@ -0,0 +1,25 @@
+#ifndef CADASTRO_H
+#define CADASTRO_H
+
+# include <QMainWindow>
+
+namespace  Ui  {
+classe  Cadastro ;
+}
+
+classe  Cadastro  :  público  QMainWindow
+{
+    Q_OBJECT
+
+públicas  ranhuras :
+    vazio  Confirma ();
+    vazio  Cancelar ();
+    anular  implementaAcaoSair ();
+
+público :
+    explícita  Cadastro ( QWidget  * pai  =  0 );
+    ~ Cadastro ();
+
+privado :
+    Ui :: Cadastro  * ui ;
+}
