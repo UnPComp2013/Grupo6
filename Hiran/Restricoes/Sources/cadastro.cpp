8f28fe41b56489a031316b547b24c09500e3741a
Hiran <gondim.game@gmail.com>
2014-10-09 18:19:40 -0300

    Update cadastro.cpp

--- Hiran/Restricoes/Sources/cadastro.cpp
+++ Hiran/Restricoes/Sources/cadastro.cpp
@@ -16,3 +16,38 @@ Cadastro::Cadastro(QWidget *parent) :
     connect(ui->btn_confirma, SIGNAL(clicked()),this, SLOT(confirma()));
     connect(ui->btn_cancelar, SIGNAL(clicked()),this, SLOT(cancelar()));
 }
+Cadastro::~Cadastro()
+{
+    delete acaoSair;
+    delete menuArquivo;
+    delete ui;
+}
+
+void Cadastro::criaAcao()
+{
+    acaoSair = new QAction(QString("&Sair"), this);
+    acaoSair->setShortcut(QString("CTRL+S"));
+    acaoSair->setStatusTip(QString("Encerra Cadastro."));
+    connect(acaoSair, SIGNAL(triggered()), this, SLOT(implementaAcaoSair()));
+
+}
+
+void Cadastro::criaMenu()
+{
+    menuArquivo = menuBar()->addMenu(QString("&Arquivo"));
+    menuArquivo->addAction(acaoSair);
+}
+
+void Cadastro::implementaAcaoSair()
+{
+    close();
+}
+
+void Cadastro::cancelar()
+{
+    close();
+}
+
+void Cadastro::confirma()
+{
+}
