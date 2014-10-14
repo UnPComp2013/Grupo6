a867e854acc8d864aba0f347a3d74f8388e60c9d
mayko <mayko_90@hotmail.com>
2014-10-14 08:32:25 -0300

    projeto

---
+++ mayko/cadastro.h
@@ -0,0 +1,27 @@
+#ifndef CADASTRO_H
+#define CADASTRO_H
+
+#include <QDialog>
+
+
+namespace Ui {
+class cadastro;
+}
+
+class cadastro : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit cadastro(QWidget *parent = 0);
+    ~cadastro();
+
+private slots:
+
+
+private:
+    Ui::cadastro *ui;
+
+};
+
+#endif // CADASTRO_H
