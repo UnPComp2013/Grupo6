357b60fcbf3452a43941ba666833d0de51cca942
maykodo <mayko_90@hotmail.com>
2014-10-15 11:02:57 -0300

    Update consulta.cpp

--- Mayko/projeto/consulta.cpp
+++ Mayko/projeto/consulta.cpp
@@ -12,3 +12,40 @@ consulta::~consulta()
 {
     delete ui;
 }
+
+void consulta::on_btn_update_clicked()
+{
+    Login conn;
+    QString curso, email, nome, senha, usuario;
+    curso = ui->ln_curso->text();
+    email = ui->ln_email->text();
+    nome = ui->ln_nome->text();
+    usuario = ui->ln_usuario->text();
+    senha = ui->ln_senha->text();
+
+    if(!conn.connOpen())
+    {
+        qDebug()<<"Failed to open the database";
+        return;
+    }
+
+    conn.connOpen();
+    QSqlQuery qry;
+    qry.prepare("Update TBL_projeto set nome='"+nome+"', curso ='"+curso+"',email='"+email+"',usuario= '"+usuario+"', senha='"+senha+"' where nome='"+nome+"'");
+
+    if(qry.exec())
+    {
+      QMessageBox::information(this, tr("Editar"), tr("atualizado com sucesso"));
+      conn.connClose();
+      this->hide();
+    }
+    else
+    {
+        QMessageBox::critical(this,tr("Error::"),qry.lastError().text());
+    }
+}
+
+void consulta::on_btn_atabela_clicked()
+{
+
+}
