fe8ef22c0aeeb150ad626df5e7f4ce7feb53baf8
maykodo <mayko_90@hotmail.com>
2014-10-15 11:03:28 -0300

    Update consulta.h

--- Mayko/projeto/consulta.h
+++ Mayko/projeto/consulta.h
@@ -2,6 +2,8 @@
 #define CONSULTA_H
 
 #include <QDialog>
+#include "login.h"
+#include <QMessageBox>
 
 namespace Ui {
 class consulta;
@@ -14,6 +16,11 @@ class consulta : public QDialog
 public:
     explicit consulta(QWidget *parent = 0);
     ~consulta();
+
+private slots:
+    void on_btn_update_clicked();
+
+    void on_btn_atabela_clicked();
 
 private:
     Ui::consulta *ui;
