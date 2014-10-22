72b4f747bbc2098f51c44a4039290aec0c0914b6
mayko <mayko_90@hotmail.com>
2014-10-22 11:53:29 -0300

    projeto

--- Mayko/projeto/consulta.cpp
+++ Mayko/projeto/consulta.cpp
@@ -15,7 +15,7 @@ consulta::~consulta()
 
 void consulta::on_btn_update_clicked()
 {
-    /*Login conn;
+    Login conn;/*
     QString curso, email, nome, senha, usuario;
     curso = ui->ln_curso->text();
     email = ui->ln_email->text();
@@ -43,11 +43,26 @@ void consulta::on_btn_update_clicked()
     {
         QMessageBox::critical(this,tr("Error::"),qry.lastError().text());
     }*/
+
+
 }
 
 void consulta::on_btn_atabela_clicked()
 {
+    Login conn;
+    QSqlQueryModel *Modal = new QSqlQueryModel();
 
+    conn.connOpen();
+    QSqlQuery *qry = new QSqlQuery(conn.mydb);
+
+    qry->prepare("select * from TBL_projeto");
+
+    qry->exec();
+    Modal->setQuery(*qry);
+    ui->tbl_coordenadores->setModel(Modal);
+
+    conn.connClose();
+    qDebug() <<(Modal->rowCount());
 }
 
 void consulta::on_btn_Cadastro_clicked()
