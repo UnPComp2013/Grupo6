3e61e70a0478b8c6dcf91b80024908202f7f0ae2
igorfel <igor_colgate@hotmail.com>
2014-10-22 04:28:17 -0300

    Update mainwindow.h

--- Igor/Header/mainwindow.h
+++ Igor/Header/mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <mainmenu.h>
 #include <QMainWindow>
 
 namespace Ui {
@@ -11,12 +12,17 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+public slots:
+    void ConfirmarLogin();
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
+    bool AtivarMenu1 = false;
     Ui::MainWindow *ui;
+    MainMenu *Menu1;
 };
 
 #endif // MAINWINDOW_H
