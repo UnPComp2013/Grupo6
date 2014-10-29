d16edb29b7902950dbafd5c75781f319f074ed01
igorfel <igor_colgate@hotmail.com>
2014-10-29 21:37:06 -0200

    Update main.cpp

--- Igor/Sources/main.cpp
+++ Igor/Sources/main.cpp
@@ -9,3 +9,27 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+
+#include <QCoreApplication>
+#include <Matrix.h>
+
+/*
+    Matrix<double> A, B;
+
+    A.add(0,0, 1);
+    A.add(1,0, 1);
+    A.add(0,1, 1);
+    A.add(1,1, 1);
+
+    B.add(0,0, 1);
+    B.add(1,0, 1);
+    B.add(0,1, 1);
+    B.add(1,1, 1);
+
+    A.print();
+    B.print();
+    (A*B).print();
+
+    return a.exec();
+}
+*/
