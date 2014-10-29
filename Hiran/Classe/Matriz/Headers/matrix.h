a065bb04351ee516a2ffadd24294d24115f1bf54
Hiran <gondim.game@gmail.com>
2014-10-29 19:58:32 -0200

    Create matrix.h

---
+++ Hiran/Classe/Matriz/Headers/matrix.h
@@ -0,0 +1,24 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+class Matrix
+{
+
+    unsigned rows, cols;
+    bool **Mat;
+
+public:
+
+    Matrix();
+    Matrix(unsigned rows, unsigned cols);
+    ~Matrix();
+    Matrix operator & (Matrix M);
+    Matrix operator = (Matrix M);
+
+    void alloc(unsigned rows, unsigned cols);
+    void Add(unsigned rows, unsigned cols, bool valor);
+    void InitMat(unsigned rows, unsigned cols);
+
+};
+
+#endif // MATRIX_H
