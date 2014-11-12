1ae44962b990e45a720da124c12e82ba525dbab8
igorfel <igor_colgate@hotmail.com>
2014-11-12 12:13:25 -0300

    Update Professor.h

--- Igor/Header/Professor.h
+++ Igor/Header/Professor.h
@@ -1,25 +1,28 @@
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
 
+#include <iostream>
 #include <matrix.h>
 #include <string>
 
-template <class TipoDaClasse>
+//template <class TipoDaClasse>
 class Professor
 {
     unsigned ID, QuantidadeDisciplinas;     //Informações do professor
-    std::string nome;
-    Matrix<TipoDaClasse> QuadroHorario, restricoes;       //Matriz de restrição e horário
+    std::string nome, Disciplinas[];
+    Matrix<bool> QuadroHorario, restricoes;       //Matriz de restrição e horário
     //Disciplina disciplinas[];
+
 public:
+    Professor();
     Professor(unsigned ID);                 //Puxa do banco as infos do professor
     ~Professor();
 
-    Matrix<TipoDaClasse> GetQuadroHorario();              //Retorna o Quadro de Horário atual
-    Matrix<TipoDaClasse> GetRestricao();                  //Retorna as restricoes de horario
+    std::string *GetDisciplinas();               //Retorna as disciplinas que o professor leciona
+    Matrix<bool> GetQuadroHorario();            //Retorna o Quadro de Horário atual
+    Matrix<bool> GetRestricao();                //Retorna as restricoes de horario
 
-    void SetQuadroHorario(Matrix<TipoDaClasse> A);        //Salva o Quadro de Horário (organizado pelo PSO)
-    //Disciplina
+    void SetQuadroHorario(Matrix<bool> A);      //Salva o Quadro de Horário (organizado pelo PSO)
 };
 
 #endif // PROFESSOR_H
