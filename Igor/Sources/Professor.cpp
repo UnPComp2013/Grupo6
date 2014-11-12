6913c9c5946bfb4b0fc12eae60706afcb5c6afd3
igorfel <igor_colgate@hotmail.com>
2014-11-12 12:16:24 -0300

    Update Professor.cpp

--- Igor/Sources/Professor.cpp
+++ Igor/Sources/Professor.cpp
@@ -1,9 +1,47 @@
 #include "professor.h"
 
-template <class TipoDaClasse>
-Professor<TipoDaClasse>::Professor()
+Professor::Professor()
 {
+    //Construtor de inicialiação, valores em branco.
+    this->ID = ID;
+    this->nome = "Esutáquio";
+    this->QuadroHorario = 0;
+    this->restricoes = NULL;
+    this->QuantidadeDisciplinas = 0;
 }
 
-template class Matrix<bool>;
-template class Matrix<double>;
+Professor::Professor(unsigned ID)
+{
+    //Fará uma busca no banco através do ID recebido.
+    //Valores Temporarios.
+    this->ID = ID;
+    this->nome = "Esutáquio";
+    this->QuadroHorario = 0;
+    this->restricoes = NULL;
+    this->QuantidadeDisciplinas = 0;
+}
+
+Professor::~Professor()
+{
+
+}
+
+std::string* Professor::GetDisciplinas()
+{
+    return this->Disciplinas;
+}
+
+Matrix<bool> Professor::GetQuadroHorario()
+{
+    return this->QuadroHorario;
+}
+
+Matrix<bool> Professor::GetRestricao()
+{
+    return this->restricoes;
+}
+
+void Professor::SetQuadroHorario(Matrix<bool> A)
+{
+    this->QuadroHorario = A;
+}
