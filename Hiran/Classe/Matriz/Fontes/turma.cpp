9b7a3654c5a7891e782e8feee9a3f401492af7e0
Hiran <gondim.game@gmail.com>
2014-11-18 14:22:18 -0200

    Create turma.cpp

---
+++ Hiran/Classe/Matriz/Fontes/turma.cpp
@@ -0,0 +1,13 @@
+#include "turma.h"
+
+Turma::Turma()
+{
+    this->nome = "";
+    this->QuantidadeDeDisciplinas = 0;
+    this->QuandtidadeDeProfessores = 0;
+    this->QuadroDeHorarioTurma = 0;
+
+    this->Disciplinas = new Disciplina[QuantidadeDeDisciplinas];
+    this->Professores = new Professor[QuantidadeDeProfessores];
+
+}
