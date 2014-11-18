d8c5ad2b77388e1b9ea968df2315adf0ce446869
Hiran <gondim.game@gmail.com>
2014-11-18 14:22:29 -0200

    Update turma.cpp

--- Hiran/Classe/Matriz/Fontes/turma.cpp
+++ Hiran/Classe/Matriz/Fontes/turma.cpp
@@ -11,3 +11,14 @@ Turma::Turma()
     this->Professores = new Professor[QuantidadeDeProfessores];
 
 }
+Turma::Turma(std::string nome)
+{
+    this->nome = nome;
+    this->QuantidadeDeDisciplinas = 0;
+    this->QuantidadeDeProfessores = 0;
+    this-> QuadroDeHorarioTurma = 0;
+
+    this->Disciplinas = new Disciplina[QuantidadeDeDisciplinas];
+    this->Professores = new Professor[QuantidadeDeProfessores];
+}
+
