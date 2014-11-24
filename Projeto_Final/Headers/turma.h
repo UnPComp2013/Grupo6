fb19d0c840f129bce41018a2fdab155df37a4757
igorfel <igor_colgate@hotmail.com>
2014-11-23 23:22:41 -0300

    Create turma.h

---
+++ Projeto_Final/Headers/turma.h
@@ -0,0 +1,30 @@
+#ifndef TURMA_H
+#define TURMA_H
+
+#include <iostream>
+#include <string>
+#include <disciplina.h>
+#include <professor.h>
+#include <primitiveLibs\matrix.h>
+
+class Turma : public Professor, public Disciplina
+{
+    std::string nome;
+    int QuantidadeDeDisciplinas, QuantidadeDeProfessores;
+
+public:
+    Turma();
+    Turma(std::string nome);
+    ~Turma();
+    int GetQuantidadeDeDisciplinas(){ return this->QuantidadeDeDisciplinas; }
+    int GetQuantidadeDeProfessores(){ return this->QuantidadeDeProfessores; }
+
+    Disciplina *Disciplinas;
+    Professor *Professores;
+    Matrix<char> QuadroDeHorarioTurma;
+    //Só poderá ser setado valores pelo banco ou pela interface do programa através do construtor
+    //void setQuantidadeDisciplinas(unsigned QuantidadeDeDisciplinas);
+    //void setQuantidadeProfessores(unsigned QuantidadeDeProfessores);
+};
+
+#endif // TURMA_H
