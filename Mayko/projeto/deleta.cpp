9bafdaa773736b5bab4afea846a0232294c391ae
mayko douglas <mayko_90@hotmail.com>
2014-11-26 13:33:21 -0200

    projeto

---
+++ Mayko/projeto/deleta.cpp
@@ -0,0 +1,14 @@
+#include "deleta.h"
+#include "ui_deleta.h"
+
+deleta::deleta(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::deleta)
+{
+    ui->setupUi(this);
+}
+
+deleta::~deleta()
+{
+deleta ui;
+}
