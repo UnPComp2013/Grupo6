3199d494c4c8f8445889f2985cea16f522e3980f
igorfel <igor_colgate@hotmail.com>
2014-11-26 09:38:15 -0300

    Update avalia.h

--- Projeto_Final/Headers/avalia.h
+++ Projeto_Final/Headers/avalia.h
@@ -1,126 +1,29 @@
-#include "avalia.h"
+#ifndef AVALIA_H
+#define AVALIA_H
 
-//------------------------Início dos------------------------//
-//----------------Construtores e Destrutores----------------//
-avalia::avalia(){}
+#include <turma.h>
+#include <primitiveLibs/matrix.h>
 
-avalia::avalia(std::string nomeTurma)
+class avalia
 {
+    int punicao;
+    Turma *turma;
+    Matrix<bool> restricoes;
 
-    this->turma = new Turma(nomeTurma);
+public:
+    avalia();
+    avalia(string nomeTurma);
+    ~avalia();
+    void organizarQuadro();
+    void restricoesDoProf();                    //Compara o quadro com as restrições do professor
+    void initQuadroHorario();
+    bool MontarQuadro(int x);
+    void classificarHoraDaDisciplina(Disciplina &disciplina);              //Divide a Disciplina dependendo da carga horaria
+    void classificarProfessorDaDisciplina(Disciplina &disciplina);    //Diz qual professor vai ministrar a disciplina
 
-    for(int i = 0; i < this->turma->GetQuantidadeDeDisciplinas(); i++)
-        classificarHoraDaDisciplina(this->turma->Disciplinas[i]);
+    double AvaliaResultadoPSO(Matrix<double>& amostragem);
+    double AvaliaResultadoPSO(Matrix<float>& amostragem);
+    Disciplina **QuadroDeHorario;          //Matriz de disciplinas
+};
 
-    initQuadroHorario();
-    organizarQuadro();
-}
-avalia::~avalia()
-{
-    for (int i = 0; i < 5; i++)
-    {
-        free(this->QuadroDeHorario[i]);
-        this->QuadroDeHorario[i] = NULL;
-    }
-    delete this->turma;
-    //free( this->turma);
-}
-//-------------------------Fim dos--------------------------//
-//----------------Construtores e Destrutores----------------//
-
-double avalia::AvaliaResultadoPSO(Matrix<double>& amostragem)
-{
-    return 1;       //aqui onde fará o tratamento do erro
-}
-
-double avalia::AvaliaResultadoPSO(Matrix<float>& amostragem)
-{
-    return 1;       //aqui onde fará o tratamento do erro
-}
-
-void avalia::initQuadroHorario()
-{
-    this->QuadroDeHorario = (Disciplina**)new Disciplina[5];
-
-    for(int i = 0; i < 5; i++)
-        this->QuadroDeHorario[i] = new Disciplina[6];
-}
-
-void avalia::classificarHoraDaDisciplina(Disciplina &disciplina)
-{
-    for(int i = 0; i < 5; i++)
-    {
-        disciplina.ocupacao1NaColuna[i] = 0;
-        disciplina.ocupacao2NaColuna[i] = 0;
-    }
-
-    if(!disciplina.getPraticaOuNao())
-    {
-        if(disciplina.getCargaHoraria() > 100 )
-        {
-            for(int i = 0; i < 5; i++)
-                disciplina.ocupacao1NaColuna[i] = 1;
-        }
-        else if(disciplina.getCargaHoraria() > 80 )
-        {
-            for(int i = 0; i < 4; i++)
-                disciplina.ocupacao1NaColuna[i] = 1;
-        }
-        else if(disciplina.getCargaHoraria() > 60 )
-        {
-            for(int i = 0; i < 3; i++)
-                disciplina.ocupacao1NaColuna[i] = 1;
-        }
-        else
-        {
-            for(int i = 0; i < 2; i++)
-                disciplina.ocupacao1NaColuna[i] = 1;
-        }
-    }
-}
-
-void avalia::classificarProfessorDaDisciplina(Disciplina &disciplina)
-{
-    for(int i = 0; i < this->turma->GetQuantidadeDeProfessores(); i++)
-        for(int j = 0; j < this->turma->Professores[i].getQuantidadeDisciplinas(); j++)
-            if(this->turma->Professores[i].GetDisciplina(j) == disciplina.getNome())
-                disciplina.setProfessor(this->turma->Professores[i]);
-}
-
-void avalia::organizarQuadro()
-{
-    int disciplinaEmQuestao = 0, horarioAtual = 0;
-
-    for(int i = 0; i < 6; i++){
-        for(int j = 0; j < 5; j++){
-            if(this->turma->Disciplinas[disciplinaEmQuestao].ocupacao1NaColuna[horarioAtual] == 1){
-                classificarProfessorDaDisciplina(this->turma->Disciplinas[disciplinaEmQuestao]);
-                this->QuadroDeHorario[j][i] = this->turma->Disciplinas[disciplinaEmQuestao];
-                this->QuadroDeHorario[j][i].setNome(this->QuadroDeHorario[j][i].getNome() + " - \n" +this->QuadroDeHorario[j][i].getProfessor().GetNome());
-
-                if(horarioAtual < 5)
-                    horarioAtual++;
-                else
-                    horarioAtual = 0;
-
-            }else{
-                if(disciplinaEmQuestao < 7)
-                    disciplinaEmQuestao++;
-
-                horarioAtual = 0;
-
-                if(this->turma->Disciplinas[disciplinaEmQuestao].ocupacao1NaColuna[horarioAtual] == 1){
-                    classificarProfessorDaDisciplina(this->turma->Disciplinas[disciplinaEmQuestao]);
-                    this->QuadroDeHorario[j][i] = this->turma->Disciplinas[disciplinaEmQuestao];
-                    this->QuadroDeHorario[j][i].setNome(this->QuadroDeHorario[j][i].getNome() + " - \n" +this->QuadroDeHorario[j][i].getProfessor().GetNome());
-
-                    if(horarioAtual < 5)
-                        horarioAtual++;
-                    else
-                        horarioAtual = 0;
-                }
-            }
-        }
-    }
-}
-
+#endif // AVALIA_H
