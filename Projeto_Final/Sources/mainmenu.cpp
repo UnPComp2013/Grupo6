5fa1aa4b9799dddb0e4fcb78f4e6db9385ffdfae
igorfel <igor_colgate@hotmail.com>
2014-11-26 13:25:40 -0300

    Update mainmenu.cpp

--- Projeto_Final/Sources/mainmenu.cpp
+++ Projeto_Final/Sources/mainmenu.cpp
@@ -14,22 +14,30 @@ MainMenu::MainMenu(QWidget *parent) :
 
 MainMenu::~MainMenu()
 {
-    //delete turma;
+    delete pso;
+    delete Av;
+    delete turma;
     delete ui;
 }
 
 void MainMenu::organizarQuadro()
 {
     QTableWidgetItem curItem;
-    //QString NomeDoProfessor;
+    QString NomeDoProfessor;
+    Matrix<double> X;
 
     this->Av = new avalia(ui->turmas->currentText().toStdString());
+    this->pso = new PSO<double>(1, 50, 50, 1011, 10, 1010, false);
+
+    X = this->pso->RunA(*this->Av);
+    curItem.setText(QString("%1").arg(X.getMat(0,0)));
 
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 6; j++){
             //NomeDoProfessor = (Av->QuadroDeHorario[i][j].getProfessor().GetNome()).c_str();
             //curItem.setText(QString((Av->QuadroDeHorario[i][j].getNome()).c_str()) +"\n"+ NomeDoProfessor);
-            curItem.setText(QString((Av->QuadroDeHorario[i][j].getNome()).c_str()));
+            //curItem.setText(QString((Av->QuadroDeHorario[i][j].getNome()).c_str()));
+            //curItem.setText(QString("%1").arg(X.getMat(0,0)));
             ui->QuadroDeHorario->setItem(i,j, curItem.clone());
         }
     }
