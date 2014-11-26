b2186b5be1d2cd2743384431fe5ec7d4ae489368
Hiran <gondim.game@gmail.com>
2014-11-25 22:07:11 -0200

    Update professor.h

--- Hiran/Classe/Matriz/cabeçalhos/professor.h
+++ Hiran/Classe/Matriz/cabeçalhos/professor.h
@@ -1,27 +1,34 @@
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
-#include <matrix.h>
+
 #include <iostream>
+#include <Headers/primitiveLibs\matrix.h>
 #include <string>
 
+//template <class TipoDaClasse>
 class Professor
 {
-    unsigned ID, QuantidadeDisciplinas;
-    std::string nome, Disciplinas[];
-    Matrix<bool> QuadroHorario, restricoes;
+    unsigned ID, QuantidadeDisciplinas;     //Informações do professor
+    std::string nome, *Disciplinas;
+    Matrix<bool> QuadroHorario, restricoes;       //Matriz de restrição e horário
+    //Disciplina disciplinas[];
 
 public:
-
     Professor();
-    Professor(unsigned ID);
+    Professor(unsigned ID);                 //Puxa do banco as infos do professor
     ~Professor();
 
-    std::string *GetDiscliplinas();
-    Matrix<bool> GetQuadroHorarios();
-    Matrix<bool> GetRestricao();
+    std::string GetNome();
+    string GetDisciplina(int Index);              //Retorna as disciplinas que o professor leciona
+    Matrix<bool> GetQuadroHorario();            //Retorna o Quadro de Horário atual
+    Matrix<bool> GetRestricao();                //Retorna as restricoes de horario
 
-    void SetQuadroHorario(Matrix<bool> A);
-
+    void SetQuadroHorario(Matrix<bool> A);      //Salva o Quadro de Horário (organizado pelo PSO)
+    void SetNome(std::string nome);
+    void SetDisciplina(std::string disciplina);
+    void SetDisciplina(unsigned QntDeDisciplinas, std::string Disciplina1, std::string Disciplina2);
+    void SetDisciplina(unsigned QntDeDisciplinas, std::string Disciplina1, std::string Disciplina2, std::string Disciplina3);
+    void SetDisciplina(unsigned QntDeDisciplinas, std::string Disciplina1, std::string Disciplina2, std::string Disciplina3, std::string Disciplina4);
 };
 
 #endif // PROFESSOR_H
