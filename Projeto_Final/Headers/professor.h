ae8f25e740961e820541d9f4594677cf9a25387f
igorfel <igor_colgate@hotmail.com>
2014-11-26 09:31:47 -0300

    Update professor.h

--- Projeto_Final/Headers/professor.h
+++ Projeto_Final/Headers/professor.h
@@ -8,23 +8,24 @@
 //template <class TipoDaClasse>
 class Professor
 {
-    unsigned ID, QuantidadeDisciplinas;     //Informações do professor
-    std::string nome, *Disciplinas;
-    Matrix<bool> QuadroHorario, restricoes;       //Matriz de restrição e horário
+    unsigned        ID,             QuantidadeDisciplinas;      //Informações do professor
+    std::string     nome,           *Disciplinas;
+    Matrix<bool>    QuadroHorario,  restricoes;                 //Matriz de restrição e horário
     //Disciplina disciplinas[];
 
 public:
     Professor();
-    Professor(unsigned ID);                 //Puxa do banco as infos do professor
+    Professor(unsigned ID);                             //Puxa do banco as infos do professor
     ~Professor();
 
-    std::string GetNome();
-    string GetDisciplina(int Index);              //Retorna as disciplinas que o professor leciona
-    Matrix<bool> GetQuadroHorario();            //Retorna o Quadro de Horário atual
-    Matrix<bool> GetRestricao();                //Retorna as restricoes de horario
+    string          GetNome();
+    string          GetDisciplina(int Index);           //Retorna as disciplinas que o professor leciona
+    Matrix<bool>    GetQuadroHorario();                 //Retorna o Quadro de Horário atual
+    Matrix<bool>    GetRestricao();                     //Retorna as restricoes de horario
+    int             getQuantidadeDisciplinas();         //Retorna a quantidade exata de disciplinas
 
-    void SetQuadroHorario(Matrix<bool> A);      //Salva o Quadro de Horário (organizado pelo PSO)
     void SetNome(std::string nome);
+    void SetQuadroHorario(Matrix<bool> A);              //Salva o Quadro de Horário (organizado pelo PSO)
     void SetDisciplina(std::string disciplina);
     void SetDisciplina(unsigned QntDeDisciplinas, std::string Disciplina1, std::string Disciplina2);
     void SetDisciplina(unsigned QntDeDisciplinas, std::string Disciplina1, std::string Disciplina2, std::string Disciplina3);
