078fc90138aaeadefe218544574f01a86724abdc
Hiran <gondim.game@gmail.com>
2014-11-25 22:07:33 -0200

    Update turma.h

--- Hiran/Classe/Matriz/cabeçalhos/turma.h
+++ Hiran/Classe/Matriz/cabeçalhos/turma.h
@@ -1,25 +1,30 @@
 #ifndef TURMA_H
 #define TURMA_H
-#include <matrix.h>
+
 #include <iostream>
 #include <string>
-#include <disciplina.h>
-#include <professor.h>
+#include <Headers/disciplina.h>
+#include <Headers/professor.h>
+#include <Headers/primitiveLibs\matrix.h>
 
-class Turma
+class Turma : public Professor, public Disciplina
 {
     std::string nome;
     int QuantidadeDeDisciplinas, QuantidadeDeProfessores;
 
-    Disciplina *Disciplinas;
-    Professor *Professores;
-    Matrix<char> QuadroDeHorarioTurma;
 public:
     Turma();
     Turma(std::string nome);
+    ~Turma();
+    int GetQuantidadeDeDisciplinas(){ return this->QuantidadeDeDisciplinas; }
+    int GetQuantidadeDeProfessores(){ return this->QuantidadeDeProfessores; }
 
-    int GetQuantidadeDeDisciplinas(){return this->QuantidadeDeDisciplinas;}
-    int GetQuantidadeDeProfessores(){return this->QuandtidadeDeProfessores;}
+    Disciplina *Disciplinas;
+    Professor *Professores;
+    Matrix<char> QuadroDeHorarioTurma;
+    //Só poderá ser setado valores pelo banco ou pela interface do programa através do construtor
+    //void setQuantidadeDisciplinas(unsigned QuantidadeDeDisciplinas);
+    //void setQuantidadeProfessores(unsigned QuantidadeDeProfessores);
 };
 
 #endif // TURMA_H
