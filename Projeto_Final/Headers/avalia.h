5d17d06caa06eb4a0b8c9888d10fd3028b530388
igorfel <igor_colgate@hotmail.com>
2014-11-26 09:33:31 -0300

    Update avalia.h

--- Projeto_Final/Headers/avalia.h
+++ Projeto_Final/Headers/avalia.h
@@ -6,18 +6,23 @@
 
 class avalia
 {
+    int punicao;
     Turma *turma;
     Matrix<bool> restricoes;
-    int punicao;
 
 public:
+    avalia();
     avalia(string nomeTurma);
     ~avalia();
-    void initQuadroHorario();
-    void classificarHoraDaDisciplina(Disciplina &disciplina);              //Divide a Disciplina dependendo da carga horaria
-    void restricoesDoProf();                    //Compara o quadro com as restrições do professor
     void organizarQuadro();
+    void restricoesDoProf();                    //Compara o quadro com as restrições do professor
+    void initQuadroHorario();
+    bool MontarQuadro(int x);
+    void classificarHoraDaDisciplina(Disciplina &disciplina);              //Divide a Disciplina dependendo da carga horaria
+    void classificarProfessorDaDisciplina(Disciplina &disciplina);    //Diz qual professor vai ministrar a disciplina
 
+    double AvaliaResultadoPSO(Matrix<double>& amostragem);
+    double AvaliaResultadoPSO(Matrix<float>& amostragem);
     Disciplina **QuadroDeHorario;          //Matriz de disciplinas
 };
 
