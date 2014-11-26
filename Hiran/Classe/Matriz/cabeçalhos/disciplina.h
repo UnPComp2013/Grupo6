65b74950d38aa3ff7948d7a45a1c9117ff750d3f
Hiran <gondim.game@gmail.com>
2014-11-25 22:06:10 -0200

    Update disciplina.h

--- Hiran/Classe/Matriz/cabeçalhos/disciplina.h
+++ Hiran/Classe/Matriz/cabeçalhos/disciplina.h
@@ -1,16 +1,37 @@
 #ifndef DISCIPLINA_H
 #define DISCIPLINA_H
+
 #include <iostream>
 #include <string>
-#include <matrix.h>
+#include <Headers/primitiveLibs/matrix.h>
+#include <Headers/professor.h>
+
 class Disciplina
 {
+    Professor professor;
     std::string nome;
-    unsigned CargaHoraria;
-    Matrix<bool> QuadroHorario;
+    int cargaHoraria;
+    bool aulaPratica;
+    //Matrix<bool> QuadroHorario;
+
+    //Professor professores;
+
 public:
     Disciplina();
-    ~Disciplina();
+    //~Disciplina();
+    std::string getNome();
+    int getCargaHoraria();
+    void setCargaHoraria(int CH);
+    void setNome(std::string nome);
+    bool getPraticaOuNao();                     //true para aula pratica, false para normal
+
+    Professor getProfessor();
+    void setProfessor(Professor professor);
+
+    int ocupacao1NaColuna[5], ocupacao2NaColuna[5];           //Define como a disciplina vai ser ocupada no quadro de horário dependendo da carga horária
+
+
 };
 
 #endif // DISCIPLINA_H
+
