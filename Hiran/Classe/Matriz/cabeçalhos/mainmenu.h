1ea13fa6800d54d4d379a6ef1d65172fea7c3de2
Hiran <gondim.game@gmail.com>
2014-11-25 22:06:42 -0200

    Create mainmenu.h

---
+++ Hiran/Classe/Matriz/cabeçalhos/mainmenu.h
@@ -0,0 +1,33 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <QMainWindow>
+#include <Headers/turma.h>
+#include <Headers/avalia.h>
+
+namespace Ui {
+class MainMenu;
+}
+
+class MainMenu : public QMainWindow
+{
+    Q_OBJECT
+    Turma *turma;
+    avalia *Av;
+
+public slots:
+    void ativar1();
+
+private slots:
+    void organizarQuadro();
+
+public:
+    explicit MainMenu(QWidget *parent = 0);
+    ~MainMenu();
+
+private:
+    Ui::MainMenu *ui;
+
+};
+
+#endif // MAINMENU_H
